1a873ec343d4e353314f5d9eccec38f159aecd0f
Johnson Lin <unlinnnn19@gmail.com>
2024-05-08 13:42:29 -0700

    update

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import dotenv
+import json
 from langchain_openai import ChatOpenAI
 
 dotenv.load_dotenv('.env')
 chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
 
+def load_profile():
+    '''load the user profile JSON format'''
+    pass
 
 
+def chatBegin():
+    pass
 
 
 
